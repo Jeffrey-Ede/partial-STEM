853bfcbfbf16fc3c11f8e85020ac564bc801f7ed
Jeffrey Ede <jeffreyede1@outlook.com>
2019-05-23 13:38:02 +0100

    Adaptive Learning Rate Clipping

--- alrc.py
+++ alrc.py
@@ -3,6 +3,10 @@ Implementation of adaptive learning rate clipping (ALRC).
 
 ALRC is applied to each loss in a batch individually. It can 
 be applied to losses with arbitrary shapes.
+
+Implementation is `clippled_loss = alrc(loss)`. Optionally, alrc hyperparameters 
+can be adjusted. Notably, performance may be improved at the start of training
+if the first raw moments of the momentum are on the scale of the losses.
 """
 
 import tensorflow as tf
@@ -22,9 +26,9 @@ def alrc(loss, num_stddev=3, decay=0.999, mu1_start=25, mu2_start=30**2):
 
     #Use capped loss for moment updates to limit the effect of extreme losses on the threshold
     sigma = tf.sqrt(mu2 - mu**2+1.e-8)
-    loss = tf.cond(loss < mu+num_stddev*sigma, 
-                   lambda: loss, 
-                   lambda: loss/tf.stop_gradient(loss/(mu+num_stddev*sigma)))
+    loss = tf.where(loss < mu+num_stddev*sigma, 
+                   loss, 
+                   loss/tf.stop_gradient(loss/(mu+num_stddev*sigma)))
 
     #Update moments
     with tf.control_dependencies([mu.assign(decay*mu+(1-decay)*loss), mu2.assign(decay*mu2+(1-decay)*loss**2)]):
